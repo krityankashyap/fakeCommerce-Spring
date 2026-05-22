232aa38cfdc9d20a59fb045292c90b0eeefef2ff
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2026-05-22 12:53:03 +0530

    Added JpaAuditing and EntityListerners

--- src/main/java/org/example/fakecommerce/FakeCommerceApplication.java
+++ src/main/java/org/example/fakecommerce/FakeCommerceApplication.java
@@ -2,8 +2,10 @@ package org.example.fakecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing  // It enables jpa auditing
 public class FakeCommerceApplication {
 
     public static void main(String[] args) {
