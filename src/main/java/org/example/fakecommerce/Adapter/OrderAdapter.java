eb3bfc156898293dd8f084bf8613335f5bf842cb
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2026-05-26 12:21:21 +0530

    Used Adapter Design Pattern In Order Service

--- src/main/java/org/example/fakecommerce/Adapter/OrderAdapter.java
+++ src/main/java/org/example/fakecommerce/Adapter/OrderAdapter.java
@@ -1,15 +1,23 @@
 package org.example.fakecommerce.Adapter;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.example.fakecommerce.Repositories.OrderProductRepository;
 import org.example.fakecommerce.dtos.GetOrderResponseDto;
 import org.example.fakecommerce.dtos.OrderItemRequestDto;
 import org.example.fakecommerce.schema.Order;
 import org.example.fakecommerce.schema.Order_Products;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Data
+@Builder
+@Component
 public class OrderAdapter {
 
     private final OrderProductRepository orderProductRepository;
--- src/main/java/org/example/fakecommerce/services/OrderService.java
+++ src/main/java/org/example/fakecommerce/services/OrderService.java
@@ -1,23 +1,38 @@
 package org.example.fakecommerce.services;
 
 import lombok.RequiredArgsConstructor;
+import org.example.fakecommerce.Adapter.OrderAdapter;
 import org.example.fakecommerce.Repositories.OrderProductRepository;
 import org.example.fakecommerce.Repositories.OrderRepository;
 import org.example.fakecommerce.Repositories.ProductRepository;
+import org.example.fakecommerce.dtos.GetOrderResponseDto;
+import org.example.fakecommerce.schema.Order;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
-@RequiredArgsConstructor
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final OrderProductRepository orderProductRepository;
     private final ProductRepository productRepository;
+    private final OrderAdapter orderAdapter;
 
-    public OrderService(OrderRepository orderRepository, OrderProductRepository orderProductRepository, ProductRepository productRepository){
+    public OrderService(OrderRepository orderRepository, OrderProductRepository orderProductRepository, ProductRepository productRepository, OrderAdapter orderAdapter){
         this.orderRepository= orderRepository;
         this.orderProductRepository= orderProductRepository;
         this.productRepository= productRepository;
+        this.orderAdapter = orderAdapter;
+    }
+
+    public List<GetOrderResponseDto> getAllOrders(){
+        List<Order> orders= orderRepository.findAll();
+
+        return orders.stream()
+                .map(order -> orderAdapter.mapToGetOrderResponseDto(order))
+                .collect(Collectors.toList());
     }
 
 
